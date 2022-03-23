7784cd09114969884645bfc07f1eb40df064f086
Saadat123456 <saadatlucky313@gmail.com>
2022-03-24 00:52:15 +0500

    Add To GitHub

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 //component file
 import TodoContainer from "./components/TodoContainer";
 import "./App.css";
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter  } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <TodoContainer />
     </BrowserRouter>
   </React.StrictMode>,
